1e4c11378dfae4ffe3666c34053dd9a09cf9f6d6
aziyan99 <rajaazian08@gmail.com>
2021-07-03 21:52:18 +0700

    add about page

---
+++ src/components/about/About.jsx
@@ -0,0 +1,100 @@
+import "./about.css";
+
+export default function About() {
+  return (
+    <div className="container">
+      <div class="box mtop-large">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus
+        neque vel tincidunt luctus. Sed dapibus enim vel condimentum
+        ullamcorper. Duis viverra eleifend quam quis mollis. Suspendisse sit
+        amet cursus ante, eu tincidunt diam. Quisque sit amet ultrices elit. Sed
+        ullamcorper tellus sed velit accumsan cursus scelerisque nec leo.
+        Phasellus hendrerit mollis mi non lobortis. Nulla at rutrum mauris,
+        luctus faucibus dui. Vivamus eu felis augue. Sed lacinia eros
+        pellentesque ante bibendum dictum. Cras feugiat rutrum tellus vitae
+        sagittis. Morbi et leo vulputate, mattis erat suscipit, congue nibh.
+        Quisque dapibus, eros ut euismod eleifend, risus tellus convallis velit,
+        nec porttitor tortor sapien ut est. Suspendisse nisl lorem, faucibus a
+        elit quis, cursus sagittis nulla. Sed bibendum, ipsum maximus vulputate
+        finibus, dui turpis pharetra felis, quis tristique leo magna ut nisl.
+        Mauris cursus nisl sed justo sodales, at viverra eros bibendum. Maecenas
+        in nisi interdum, porttitor lorem quis, condimentum dui. Nam a malesuada
+        purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+        posuere cubilia curae; Vestibulum eleifend dignissim felis, non lobortis
+        tellus dictum vitae. Nulla sit amet consectetur magna. Suspendisse dui
+        lorem, dignissim ullamcorper varius quis, imperdiet a risus. Cras porta
+        nulla quis commodo pretium. Praesent eget turpis est. Aenean convallis
+        venenatis risus, ut feugiat nulla imperdiet vel. Aliquam vehicula dolor
+        vitae ultrices rutrum. Suspendisse hendrerit, nisl vel rutrum dictum,
+        velit libero suscipit sapien, sit amet tristique odio diam at felis.
+        Aliquam laoreet facilisis ultricies. Quisque quis leo pretium arcu
+        semper fermentum. Ut ante lorem, hendrerit a dignissim ac, porta ac
+        elit. Fusce eu nisi ut velit scelerisque vulputate. Donec eu risus
+        velit. Donec facilisis ultricies rutrum. Phasellus viverra orci eu
+        aliquet consequat. Etiam ullamcorper condimentum lectus ac sollicitudin.
+        Vivamus tempor lobortis urna, volutpat maximus dolor imperdiet eget.
+        Donec congue in risus id tempus. Duis a condimentum magna. Proin blandit
+        dictum urna in blandit. Maecenas dapibus metus erat, ut vulputate ipsum
+        venenatis vitae. Duis pulvinar eros eu dapibus rutrum. Integer eget
+        tristique metus, vitae pellentesque nisi. Maecenas euismod pellentesque
+        tincidunt. Vestibulum semper ante ac iaculis ornare. Vestibulum gravida
+        egestas orci, vitae consectetur sem tempus cursus. Vestibulum ante ipsum
+        primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin
+        fermentum tortor quis ex mollis, nec volutpat dui condimentum. Sed et
+        tellus ex. Proin ultrices odio augue, malesuada laoreet libero commodo
+        nec. Phasellus tempus vitae erat mollis imperdiet. Sed imperdiet lacus
+        mauris, ac convallis odio lobortis in. Praesent nec nunc ullamcorper
+        ante varius placerat vitae a sapien. Donec at odio et odio ultricies
+        vehicula. In nisl mauris, sollicitudin eget tincidunt ac, gravida eu
+        sapien. Sed elementum massa dolor, quis mattis sapien fringilla
+        volutpat. Proin justo lectus, vestibulum in sagittis viverra, tincidunt
+        et ex. Vivamus erat odio, auctor a dolor ut, bibendum pulvinar augue.
+        Nulla consequat pellentesque dolor nec dapibus. Aliquam nec elit ornare,
+        eleifend libero id, pretium magna. Nulla feugiat mauris et ante
+        placerat, in suscipit tortor sodales. Nam vel iaculis purus. Donec
+        placerat neque lorem, nec ullamcorper neque lacinia nec. Duis velit
+        risus, sollicitudin vel venenatis vitae, gravida nec turpis. Maecenas
+        vitae ex urna. Proin ornare malesuada urna, in suscipit dolor faucibus
+        vel. Suspendisse elementum tellus a nunc vestibulum luctus. Nam egestas,
+        leo ac vehicula imperdiet, quam tortor pellentesque justo, nec sodales
+        erat tortor eu diam. Suspendisse dapibus iaculis metus. Sed eros lacus,
+        finibus at accumsan ac, placerat at tellus. Ut facilisis auctor ex.
+        Fusce et ex vel velit rhoncus ullamcorper. Pellentesque lorem libero,
+        rutrum sit amet velit et, fermentum interdum leo. Sed sit amet gravida
+        felis, at rutrum dui. Vivamus condimentum magna mauris, et tristique
+        lacus iaculis scelerisque. Donec eu convallis enim. Maecenas purus
+        metus, semper ac ornare id, suscipit nec mauris. Aenean volutpat nibh
+        ligula, eu aliquet nibh maximus sit amet. Aliquam erat volutpat. Donec
+        ornare elit non molestie tempus. Nam porttitor lectus ut dui porttitor,
+        eu hendrerit magna auctor. Etiam id accumsan metus. Vestibulum ante
+        ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        Sed lectus dolor, luctus vel odio at, tempor condimentum massa. Aliquam
+        ac sem eget nunc tincidunt suscipit. Sed id arcu lobortis tortor tempus
+        lobortis. Maecenas finibus sit amet orci eget efficitur. Nam vulputate,
+        dui eu elementum vehicula, magna orci suscipit augue, eget finibus odio
+        magna vitae nulla. Nulla facilisi. Curabitur dignissim ex finibus lectus
+        pharetra tristique. Phasellus sollicitudin nulla non purus dignissim,
+        sodales tincidunt velit mattis. Maecenas euismod, enim ac iaculis
+        elementum, augue nisl pellentesque libero, at posuere augue diam ut
+        velit. Mauris aliquet dolor arcu, a condimentum est lacinia vitae. Nulla
+        massa nulla, aliquam ac ex non, hendrerit condimentum mauris. Nam
+        sodales vulputate tincidunt. Aenean at eros sapien. Aenean sit amet
+        finibus lectus. Nulla consequat pulvinar diam, eu consectetur ex posuere
+        at. Vestibulum blandit sagittis bibendum. Orci varius natoque penatibus
+        et magnis dis parturient montes, nascetur ridiculus mus. Vivamus id leo
+        id augue maximus volutpat vel non lorem. Phasellus quis suscipit arcu.
+        Suspendisse fermentum auctor gravida. Duis enim sapien, tincidunt
+        laoreet odio eget, lobortis mattis nulla. Ut aliquam risus lacus, sit
+        amet gravida sem hendrerit id. Nulla eleifend eros a nunc facilisis
+        vehicula. Sed lobortis eros felis. Morbi ex mauris, efficitur eget
+        interdum id, tincidunt volutpat diam. Mauris vitae auctor erat.
+        Curabitur turpis erat, egestas ac lacus ut, elementum placerat elit.
+        Maecenas non tellus magna. Nullam feugiat tortor ac sem euismod
+        facilisis. Vestibulum id justo pellentesque, laoreet felis vel,
+        tincidunt lacus. Curabitur vestibulum accumsan odio vel euismod. Ut at
+        viverra justo. Etiam mi lectus, mollis vel porta nec, maximus maximus
+        ex.
+      </div>
+    </div>
+  );
+}
